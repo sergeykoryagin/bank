260990bb5f6b3379a5feee48d251f25570db3fbb
VasilyLapinsky <vasia2001lapa3251@gmail.com>
2022-02-26 18:52:38 +0300

    implemented app service methods

--- bank-server/src/app.service.ts
+++ bank-server/src/app.service.ts
@@ -1,31 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { hostname } from 'os';
 import { Room } from './game/room';
+import { BankSettings } from './interfaces/bank-settings';
+import { Settings } from './interfaces/settings';
 
 @Injectable()
 export class AppService {
-    private rooms: Room[];
+    private rooms: Room[] = [];
 
-    createRoom(idHost: number, nameHost: string, userStartMoney: number, bankStartMoney: number) {
-
+    findRoom(id: string): Room | undefined {
+        return this.rooms.find((room) => id === room.id);
     }
 
-    startGame(idGame: number) {
-
-    }
-
-    joinGame(idGame: number, idUser: number, nameUser: string) {
-
-    }
-
-    doTransaction(idGame: number, idSender: number, idReceiver: number, amount: number) {
-
-    }
-
-    getMoneyFromBank(idGame: number, idUser: number, amount: number) {
-
-    }
-
-    sendMoneyFromBank(idGame: number, idUser: number, amount: number) {
+    createRoom(idHost: number, hostname: string, userStartMoney: number, bankStartMoney: number) {
+        let bankSettings: BankSettings = {bankStartMoney: bankStartMoney};
+        let settings: Settings = {startMoney: userStartMoney, idHost: idHost, bankSettings: bankSettings};
         
+        this.rooms.push(new Room(settings, hostname));
+    }
+
+    startGame(idGame: string) {
+        this.findRoom(idGame)?.startGame();
+    }
+
+    joinGame(idGame: string, playerName: string) {
+        this.findRoom(idGame)?.addPlayer(playerName);
+    }
+
+    doTransaction(idGame: string, idSender: string, idReceiver: string, amount: number) {
+        this.findRoom(idGame)?.sendMoneyToPlayer(idSender, idReceiver, amount);
+    }
+
+    getMoneyFromBank(idGame: string, idUser: string, amount: number) {
+        this.findRoom(idGame)?.getMoneyFromBank(idUser, amount);
+    }
+
+    sendMoneyToBank(idGame: string, idUser: string, amount: number) {
+        this.findRoom(idGame)?.sendMoneyToBank(idUser, amount);
     }
 }
