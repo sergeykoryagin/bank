3ecb21aa4e511659e048516eb8069494b25d36ff
VasilyLapinsky <vasia2001lapa3251@gmail.com>
2022-02-26 18:03:57 +0300

    add: app service methods

--- bank-server/src/app.service.ts
+++ bank-server/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Room } from './game/room';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World! kek';
+    private rooms: Room[];
+
+    createRoom(idHost: number, nameHost: string, userStartMoney: number, bankStartMoney: number) {
+
+    }
+
+    startGame(idGame: number) {
+
+    }
+
+    joinGame(idGame: number, idUser: number, nameUser: string) {
+
+    }
+
+    doTransaction(idGame: number, idSender: number, idReceiver: number, amount: number) {
+
+    }
+
+    getMoneyFromBank(idGame: number, idUser: number, amount: number) {
+
+    }
+
+    sendMoneyFromBank(idGame: number, idUser: number, amount: number) {
+        
     }
 }
